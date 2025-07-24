d5e172574d1f67f282f44f886984578dda2adc6a
RaghushettyK <raghukrishna043@gmail.com>
2025-07-25 02:16:58 +0530

    Update useGetCompanyById.jsx

--- frontend/src/hooks/useGetCompanyById.jsx
+++ frontend/src/hooks/useGetCompanyById.jsx
@@ -3,7 +3,7 @@ import { setAllJobs } from '@/redux/jobSlice'
 import { COMPANY_API_END_POINT, JOB_API_END_POINT } from '@/utils/constant'
 import axios from 'axios'
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+
 
 const useGetCompanyById = (companyId) => {
     const dispatch = useDispatch();
